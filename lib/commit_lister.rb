2f6b12be6d3fddff4808697ddad3ef399cc57a10
Nuttanart Pornprasitsakul <visibletrap@gmail.com>
2012-05-26 13:50:59 +0800

    CommitLister ignore commit that doesn't have story id

--- lib/commit_lister.rb
+++ lib/commit_lister.rb
@@ -1,7 +1,11 @@
 class CommitLister
   def list(since, until_commit)
-    raw_commits = git_list_commit(since, until_commit).split("\n")
-    Hash[raw_commits.reverse.map {|c| [/^\w*/.match(c)[0], /\[#(\d*)\]/.match(c)[1]] }]
+    commits = git_list_commit(since, until_commit).split("\n")
+    commits_with_story = commits.reject { |c| /\[#(\d*)\]/.match(c).nil? }
+    commit_story = commits_with_story.reverse.map do |c|
+      [/^\w*/.match(c)[0], /\[#(\d*)\]/.match(c)[1]]
+    end
+    Hash[commit_story]
   end
 
   def git_list_commit(since, until_commit)
--- spec/commit_lister_spec.rb
+++ spec/commit_lister_spec.rb
@@ -2,12 +2,10 @@ require '../lib/commit_lister'
 
 describe CommitLister do
 
-  it "has #git_list_commit" do
-    CommitLister.new.should respond_to(:git_list_commit)
-  end
+  it { should respond_to(:git_list_commit) }
 
   describe "#list" do
-    it "list commit from after since_commit to until_commit and return as a hash { commit => story }" do
+    it "lists commits from after since_commit to until_commit as a hash { commit => story }" do
       since = "941a3e6b34"
       until_commit = "13351e6"
       raw_commit = <<raw_git_commit
@@ -16,14 +14,28 @@ describe CommitLister do
 5257d758a95d37441e65d0b5198cbc9dae8cd2cf [#29521391] Episode index and Episode show page
 8670b5ee48d9ea26a91b22f6eeee2234098b3a08 [#29981485] Hardcode country filters for music landing page
 raw_git_commit
-      cl = CommitLister.new
-      cl.stub(:git_list_commit).and_return(raw_commit)
-      result = cl.list(since, until_commit)
-      result.should ==
+      subject.stub(:git_list_commit).and_return(raw_commit)
+
+      subject.list(since, until_commit).should ==
           {
               '8670b5ee48d9ea26a91b22f6eeee2234098b3a08' => '29981485',
               '5257d758a95d37441e65d0b5198cbc9dae8cd2cf' => '29521391',
               '70c9112ddf9f02e6680797f490e418d95a3836ed' => '29977427',
+              '13351e6dfc28e05cc4c13ea039654b95c62185a0' => '29977409'
+          }
+    end
+
+    it "ignores commit without story id" do
+      since = "941a3e6b34"
+      until_commit = "13351e6"
+      raw_commit = <<raw_git_commit
+13351e6dfc28e05cc4c13ea039654b95c62185a0 [#29977409] Filtered by platform
+70c9112ddf9f02e6680797f490e418d95a3836ed filter series by genre and country_o
+raw_git_commit
+      subject.stub(:git_list_commit).and_return(raw_commit)
+
+      subject.list(since, until_commit).should ==
+          {
               '13351e6dfc28e05cc4c13ea039654b95c62185a0' => '29977409'
           }
     end
